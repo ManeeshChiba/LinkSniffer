e26a6ccf9b621092266433fb35110ebf78e90142
Maneesh <mpchiba@gmail.com>
2015-07-20 20:14:31 +0200

    Adding in identifiers.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-window.onload = function () {
+var hostname = window.location.hostname;
+var inHost = new RegExp('^(\/|\#)',"gi");
+var inReferance = /^(\/|#)$/;
+$(function(){
+	$('a').each(function(){
+		var currentHref = $(this).attr('href');
 
-}
+		// if ( inReferance.test($(this).attr('href').charAt(0)) ){
+		// 	$(this).addClass('internal');
+		// }
+		if ( inHost.test($(this).attr('href')) ){
+			$(this).addClass('internal');
+		}
+	});
+});
